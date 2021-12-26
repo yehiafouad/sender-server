dca933dbc932c2a11ddbed0d68a03e479bbb988e
yehiafouad <yehiafouad.yf@gmail.com>
2021-12-26 11:52:20 +0200

    inital commit

--- src/db/rabbitMQ.connection.js
+++ src/db/rabbitMQ.connection.js
@@ -2,29 +2,34 @@ const ampq = require("amqplib/callback_api");
 
 const sendToQueue = async (data, queueName) => {
   let result;
-  ampq.connect("amqp://localhost", (err, connection) => {
-    if (err) {
-      throw err;
-    }
-
-    // Create Channel
-    connection.createChannel(async (chErr, channel) => {
-      if (chErr) {
-        throw chErr;
+  ampq.connect(
+    process.env.NODE_ENV === "development"
+      ? "amqp://localhost"
+      : process.env.RABBITMQ_URL,
+    (err, connection) => {
+      if (err) {
+        throw err;
       }
 
-      // Assert Queue
-      const QUEUE = queueName;
-      channel.assertQueue(QUEUE, { durable: false });
+      // Create Channel
+      connection.createChannel(async (chErr, channel) => {
+        if (chErr) {
+          throw chErr;
+        }
 
-      // Send Message to queue
-      channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(data)));
-      // console.log("Message Send");
-      // console.log(QUEUE);
-      // result = await receiveMessage(QUEUE);
-      // return result
-    });
-  });
+        // Assert Queue
+        const QUEUE = queueName;
+        channel.assertQueue(QUEUE, { durable: false });
+
+        // Send Message to queue
+        channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(data)));
+        // console.log("Message Send");
+        // console.log(QUEUE);
+        // result = await receiveMessage(QUEUE);
+        // return result
+      });
+    }
+  );
 
   process.on("beforeExit", () => {
     connection.close();
